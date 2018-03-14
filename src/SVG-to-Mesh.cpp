f9407044ce93742096d467ce6d61bd850eb7a5a9
Christoph Neuhauser <christoph.neuhauser@tum.de>
2018-03-14 16:11:10 +0100

    Initial commit

---
+++ src/SVG-to-Mesh.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : SVG-to-Mesh.cpp
+// Author      : Christoph Neuhauser
+// Version     : 0.1
+// Copyright   : GNU GPLv3 (for more details see LICENSE file)
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+}
